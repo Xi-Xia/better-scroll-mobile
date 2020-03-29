295d6ccb382b69ded1c68601c2f744b0dec86685
Xi-Xia <171246583@qq.com>
2020-03-29 19:10:59 +0800

    'update'

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+
+<template>
+    <div class="app">
+        <div class="scroll-box">
+            <scroll />
+        </div>
+    </div>
+</template>
+
+<script>
+import scroll from 'components/Scroll.vue'
+export default {
+    name:'App',
+    components:{
+        scroll
+    },
+    data(){
+        return{}
+    }
+}
+</script>
+
+<style lang="less" scoped>
+    .app{
+        padding: 5px;
+        .scroll-box{
+            padding: 4px 0;
+            border-radius: 4px;
+            border: solid 1px cyan;
+        }
+    }
+</style>
